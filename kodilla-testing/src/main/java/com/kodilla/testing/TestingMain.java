87a1a558c0a505fb4e404728f937472f00518416
“silverwind85 <slcieslar@gmail.com>
2017-10-15 11:34:22 +0200

    First test calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calaculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("First test Calculator");
+        System.out.println("Testing first method add");
+        Calculator calculator = new Calculator();
+        final int A = 9;
+        final int B = 10;
+        int resultAdd = A + B;
+        int calculatorAdd = calculator.add(A, B);
+        if (resultAdd == calculatorAdd) {
+            System.out.println("test ok");
+        } else {
+            System.out.println("Error");
+        }
+        System.out.println("Testing second method subtract");
+        int resultSubtract = A - B;
+        int calculatorSubtract = calculator.subtract(A, B);
+        if (resultSubtract == calculatorSubtract) {
+            System.out.println("test ok");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
