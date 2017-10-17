203f3f3db41cb1c62acf4cababd9509a9a35e91e
“silverwind85 <slcieslar@gmail.com>
2017-10-17 19:29:49 +0200

    another Commit ShapeCollector

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -17,7 +17,7 @@ public class ShapeCollector {
     }
 
     public Shape getFigure(int n) {
-        if (n <= 0 || n > shapes.size()) {
+        if (n < 0 || n > shapes.size()) {
             return null;
         }
         return shapes.get(n);
