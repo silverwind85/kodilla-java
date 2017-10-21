7744239d1753b070e585692333ece5b4f21d6114
“silverwind85 <slcieslar@gmail.com>
2017-10-21 19:00:43 +0200

    First Commit AverageSum

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,7 +4,12 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
+
+        IntStream.range(0, numbers.length)
+                .mapToDouble(a -> numbers[a])
+                .forEach(a-> System.out.println(a));
         return IntStream.range(0, numbers.length)
                 .mapToDouble(a -> numbers[a]).average().getAsDouble();
+
     }
 }
