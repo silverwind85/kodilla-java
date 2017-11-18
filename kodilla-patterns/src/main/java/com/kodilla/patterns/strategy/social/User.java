14afa388b2d72fcaaaadd8bfca1029ae0691c335
“silverwind85 <slcieslar@gmail.com>
2017-11-18 10:46:40 +0100

    First Commit 11.3 submodule

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -0,0 +1,17 @@
+package com.kodilla.patterns.strategy.social;
+
+public class User {
+    private String name;
+    protected SocialPublisher socialPublisher;
+
+    public User(String name) {
+        this.name = name;
+    }
+    public String sharePost(){
+        return socialPublisher.share();
+    }
+    public void setSocialMedia(SocialPublisher socialPublisher){
+        this.socialPublisher = socialPublisher;
+    }
+
+}
